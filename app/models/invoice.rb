3950c568ecc484cf2807b029fc168f861faa1b4a
Juliet Eyraud <juliet@turing.edu>
2024-08-26 13:05:49 -0500

    Add transaction and dependent destroy for invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
   has_many :invoice_items
+  has_many :transactions, dependent: :destroy
 
   validates :status, inclusion: { in: ["shipped", "packaged", "returned"] }
 end
