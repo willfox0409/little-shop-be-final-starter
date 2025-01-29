49947d9228ea04d796afc08ecd8b2897a637afbd
Abdul Redd <104014874+abdulredd@users.noreply.github.com>
2025-01-29 07:59:08 -0500

    update tests to 100% coverage

--- spec/requests/api/v1/merchant/invoices_request_spec.rb
+++ spec/requests/api/v1/merchant/invoices_request_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe "Merchant invoices endpoints" do
     @customer2 = Customer.create!(first_name: "Jimmy", last_name: "John")
 
     @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "packaged")
-    Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
-    Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
-    Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
-    @invoice2 = Invoice.create!(customer: @customer1, merchant: @merchant2, status: "shipped")
+    @invoice2 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
+    @invoice3 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
+    @invoice4 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
+    @invoice5 = Invoice.create!(customer: @customer1, merchant: @merchant2, status: "shipped")
   end
 
   it "should return all invoices for a given merchant based on status param" do
@@ -45,7 +45,7 @@ RSpec.describe "Merchant invoices endpoints" do
 
     expect(response).to be_successful
     expect(json[:data].count).to eq(1)
-    expect(json[:data][0][:id]).to eq(@invoice2.id.to_s)
+    expect(json[:data][0][:id]).to eq(@invoice5.id.to_s)
   end
 
   it "should return 404 and error message when merchant is not found" do
@@ -57,5 +57,15 @@ RSpec.describe "Merchant invoices endpoints" do
     expect(json[:message]).to eq("Your query could not be completed")
     expect(json[:errors]).to be_a Array
     expect(json[:errors].first).to eq("Couldn't find Merchant with 'id'=100000")
+  end
+
+  it "should return all invoices for a given merchant without status filter" do
+    get "/api/v1/merchants/#{@merchant1.id}/invoices"
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(json[:data].count).to eq(4)
+    expect(json[:data].map { |invoice| invoice[:id] }).to match_array([@invoice1.id.to_s, @invoice2.id.to_s, @invoice3.id.to_s, @invoice4.id.to_s])
   end
 end
