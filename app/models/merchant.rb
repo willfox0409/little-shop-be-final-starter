b1938457fe10340688966b8fccb169398b4bc89c
Will Fox <william.macdonald.fox@gmail.com>
2025-03-07 17:54:31 -0500

    feat: add has_many :coupons association to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,7 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
   has_many :customers, through: :invoices
+  has_many :coupons
   # has_many :invoice_items, through: :invoices
   # has_many :transactions, through: :invoices
 
