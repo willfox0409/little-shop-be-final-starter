3e6f5a24a836d275a585292f5c34468af3145e58
Will Fox <william.macdonald.fox@gmail.com>
2025-03-07 17:59:04 -0500

    feat: add belongs_to :coupon association in invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+  belongs_to :coupon
   has_many :invoice_items, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
