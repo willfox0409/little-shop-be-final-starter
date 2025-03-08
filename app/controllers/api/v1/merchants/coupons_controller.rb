f604f2b898a550b05f1ecc8617ed903bf189439a
Will Fox <william.macdonald.fox@gmail.com>
2025-03-08 16:29:54 -0500

    feat: create requests/merchants/coupon_spec file

---
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::Merchants::CouponsController < ApplicationController
+    before_action :merchant_setup
+
+  def index
+    coupons = @merchant.coupons
+    render json: CouponSerializer.new(coupons)
+  end
+
+  def show
+    coupon = @merchant.coupons.find(params[:id])
+    render json: CouponSerializer.new(coupon)
+  end
+
+  def create
+    coupon = @merchant.coupons.create!(coupon_params) 
+    render json: CouponSerializer.new(coupon), status: :created
+  end
+
+  def update
+    coupon = @merchant.coupons.find(params[:id])
+    coupon.update!(coupon_params)
+
+    render json: CouponSerializer.new(coupon)
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :code, :discount_value, :discount_type, :active)
+  end
+
+  def merchant_setup
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+end
