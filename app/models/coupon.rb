2ea27bfdb34031d6b0969da53c8d0d0436595e24
Will Fox <william.macdonald.fox@gmail.com>
2025-03-08 16:57:21 -0500

    fix: correct syntax error in coupon validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,8 +2,8 @@ class Coupon < ApplicationRecord
     belongs_to :merchant
     has_many :invoices
 
-    validates :name, presence :true
-    validates :code, presence :true
-    validates :discount_value, presence :true, numericality: { greater_than: 0 }
-    validates :discount_type, presence :true
+    validates :name, presence: true
+    validates :code, presence: true
+    validates :discount_value, presence: true, numericality: { greater_than: 0 }
+    validates :discount_type, presence: true
 end
