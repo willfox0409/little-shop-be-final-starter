acdb98e9a8296e4d0e171f7848611c36f4cc6b7b
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 13:08:35 -0400

    fix: update invoice model to optionally belong_to a coupon, instead of automatically defaulting

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
-  belongs_to :coupon
+  belongs_to :coupon, optional: true
   has_many :invoice_items, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
