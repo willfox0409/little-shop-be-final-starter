a4f29f09b0b338b29b8acd9de7d658da726db635
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 18:31:22 -0400

    test: add test for custom validation in invoice model spec, an inactive coupon cannot be added to an invoice and raises a graceful error

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Invoice do
   it { should belong_to :merchant }
   it { should belong_to :customer }
   it { should validate_inclusion_of(:status).in_array(%w(shipped packaged returned)) }
+
+  describe 'custom validations' do
+    it "should not allow an inactive coupon to be applied" do
+      merchant = create(:merchant)
+      customer = create(:customer)
+      inactive_coupon = create(:coupon, merchant: merchant, active: false)  
+
+      invoice = Invoice.new(customer: customer, merchant: merchant, status: "packaged", coupon: inactive_coupon)
+
+      expect(invoice.valid?).to be false 
+      expect(invoice.errors[:coupon_id]).to include("Coupon is not active and cannot be applied")
+    end
+  end
 end
