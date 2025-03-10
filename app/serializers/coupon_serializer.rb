78e8b8b738ca6af8dd14ccc72bd8b44c74433143
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 10:16:32 -0400

    feat: add usage_count to coupon serializer

--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -1,4 +1,4 @@
 class CouponSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :code, :discount_value, :discount_type, :active, :merchant_id
+  attributes :id, :name, :code, :discount_value, :discount_type, :active, :merchant_id, :usage_count
 end
