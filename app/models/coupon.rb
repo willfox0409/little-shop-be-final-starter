4878af01a9044df86f9d89152cd5617f99ecc5e1
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 17:01:45 -0400

    fix: allow nil as a value on model level in coupon validations for usage_count

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -6,7 +6,7 @@ class Coupon < ApplicationRecord
     validates :code, presence: true
     validates :discount_value, presence: true, numericality: { greater_than: 0 }
     validates :discount_type, presence: true
-    validates :usage_count, numericality: { greater_than_or_equal_to: 0 }
+    validates :usage_count, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 
     attribute :usage_count, :integer, default: 0
 
