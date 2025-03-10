0ce5db4cf1223b074337a91c3f2fe673e5305473
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 17:22:07 -0400

    feat: add custom validation method max_active _coupons to list of validations on :create action, add uniqueness rule to :code validation

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,14 +3,24 @@ class Coupon < ApplicationRecord
     has_many :invoices
 
     validates :name, presence: true
-    validates :code, presence: true
+    validates :code, presence: true, uniqueness: { case_sensitive: false }
     validates :discount_value, presence: true, numericality: { greater_than: 0 }
     validates :discount_type, presence: true
     validates :usage_count, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+    validate :max_active_coupons, on: :create #custom validation method
 
     attribute :usage_count, :integer, default: 0
 
     def increment_usage!
         update!(usage_count: (usage_count || 0) + 1)
+    end
+
+    private
+
+    def max_active_coupons
+      if merchant.coupons.where(active: true).count >= 5
+        errors.add(:base, "Merchant cannot have more than 5 active coupons")
+      end
     end
 end
