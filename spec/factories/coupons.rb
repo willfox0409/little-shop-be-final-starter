0f8513ded28766e9f6614f84052ceeca8b9fd241
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 10:15:11 -0400

    feat: update coupons in factory bot

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   association :merchant
     name { Faker::Commerce.promotion_code }
     code { Faker::Alphanumeric.alphanumeric(number: 6).upcase }
-    discount_calue { rand(5...50) }
+    discount_value { rand(5...50) }
     discount_type { ["dollar", "percent"].sample }
     active { ["true", "false"].sample }
     created_at { Time.current }
