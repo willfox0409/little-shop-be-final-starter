a35642d558f4ccdae79f42543ed7d7b5f5a4ffec
Will Fox <william.macdonald.fox@gmail.com>
2025-03-10 10:16:10 -0400

    feat: check for updated schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_07_212307) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_09_211210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_07_212307) do
     t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "usage_count", default: 0, null: false
     t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
